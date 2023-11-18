dc18d9c4348670c7a6ec045633cc182f4966ec66
Robin Münk <RobinMnk@users.noreply.github.com>
2023-11-18 16:56:02 +0100

    begin parallelism

--- server/bridge/craft-rank/src/db_reader/database_reader.h
+++ server/bridge/craft-rank/src/db_reader/database_reader.h
@@ -7,6 +7,13 @@ struct ZipCodeInfo {
     int distance{40}; // Neighbour distance
 };
 
+struct WorkerInfo {
+    float lat{0}; // Latitude in radians
+    float lon{0}; // Longitude in radians
+    int maxDrivingDistance{40}; // Neighbour distance
+    float profileScore{0};
+};
+
 namespace db {
 
     void getNeighboringZips(int zipCode, const std::vector<int>& neighbors);
@@ -14,5 +21,7 @@ namespace db {
     void allWorkersForZips(const std::vector<int>& zips, const std::vector<int>& workers);
 
     ZipCodeInfo getZipInfo(int zipCode);
+
+    WorkerInfo getWorkerInfo(int workerId);
 
 }
